031c75614068bb14a19581cfc102e6a48fe045c6
flawgical <shahzadkhan3iii7@gmail.com>
2021-12-01 10:29:45 -0600

    shopping cart starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import useState
+import React, { useState } from 'react';
+import './styles.css';
+import AllTheThings from './components/AllTheThings';
+import MyShoppingCart from './components/MyShoppingCart';
+import Form from './components/Form';
+import productsArr from './products';
 
-function App() {
+export default function App() {
+  const [products, setProducts] = useState(productsArr);
+  const [cart, setCart] = useState([]);
+
+  // create an addToCart function that takes in a product as a param
+  // using the ...spread operator add the product to the cart array
+
+  // create an removeFromCart function that takes in an index as a param
+  // using Array.filter remove create a new array where that item is removed
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Big Time Shopping</h1>
+      <Form />
+      <div className="products">
+        <AllTheThings />
+        <MyShoppingCart />
+      </div>
     </div>
   );
 }
-
-export default App;
